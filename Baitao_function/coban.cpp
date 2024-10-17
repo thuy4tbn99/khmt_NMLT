dad6c27975f4f7ca2eced2e65135fe54aae9be21
thuy4tbn99 <thuy4tbn99>
2024-10-17 14:30:12 +0700

    add bt function

--- Baitao_function/coban.cpp
+++ Baitao_function/coban.cpp
@@ -2,22 +2,55 @@
 
 using namespace std;
 
-void doicho_2(int &a, int &b){
-    int tmp = a;
-    a = b;
-    b = tmp;
+
+int tong(int a, int b){
+    int sum = a+b;
+    return sum;
 }
 
-void doicho(int a, int b){
-    int tmp = a;
-    a = b;
-    b = tmp;
+void hello(){
+    cout << "hello world";
 }
+
+float increase(float x){
+    x = x+0.5;
+    return x;
+}
+
+void increase_ver2(float x){
+    x = x+0.5;
+}
+
+void increase_ver3(float &x){
+    x = x+0.5;
+}
+
+
+
+//void doicho_2(int &a, int &b){
+//    int tmp = a;
+//    a = b;
+//    b = tmp;
+//}
+//
+//void doicho(int a, int b){
+//    int tmp = a;
+//    a = b;
+//    b = tmp;
+//}
 
 int main(){
-    int var1 = 3, var2 = 4
-    doicho(var1, var2);
-    cout << var1 << " " << var2;
+//    int a = 4, b=3;
+//    int sum_ab = tong(a,b);
+//    cout << sum_ab;
+
+    float bien1 = 4.3;
+    bien1 = increase(bien1);
+    cout << bien1;
+
+//    int var1 = 3, var2 = 4
+//    doicho(var1, var2);
+//    cout << var1 << " " << var2;
 
     return 0;
 }
